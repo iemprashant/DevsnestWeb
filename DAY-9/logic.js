eaa7303c3134157e8df9d8a9a7e71eb1fd97fc50
iemprashant <prashantgupta9876@gmail.com>
2021-06-22 14:43:39 +0530

    updated THA-9

--- DAY-9/logic.js
+++ DAY-9/logic.js
@@ -1,26 +1,23 @@
-var remainingVar = 36
+var remainingVar = 120
 var bookedVar = 0
-var remainingSeats = document.querySelector('.remainingSeats')
 var bookedSeats = document.querySelector('.bookedSeats')
 const gridContainer = document.querySelector('.grid-container')
-for (let i = 1; i <= 36; i++) {
+for (let i = 1; i <= 120; i++) {
   const divbox = document.createElement('div')
-  divbox.classList.add('pixel')
+  divbox.classList.add('box')
   divbox.id = `${i}`
   gridContainer.appendChild(divbox)
   divbox.addEventListener('click', (e) => {
     if (e.target.classList.contains('checked')) {
       e.target.classList.remove('checked')
       bookedVar--
-      bookedSeats.innerHTML = 'Booked Seats : ' + bookedVar
       remainingVar++
-      remainingSeats.innerHTML = 'Remaining Seats : ' + remainingVar
+      bookedSeats.innerHTML = bookedVar
     } else {
       e.target.classList.add('checked')
       bookedVar++
-      bookedSeats.innerHTML = 'Booked Seats : ' + bookedVar
       remainingVar--
-      remainingSeats.innerHTML = 'Remaining Seats : ' + remainingVar
+      bookedSeats.innerHTML = bookedVar
     }
   })
 }
