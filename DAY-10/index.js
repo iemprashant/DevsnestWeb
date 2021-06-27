0731772aa8bc6153a4665c1ec61fff7009d4fd51
iemprashant <prashantgupta9876@gmail.com>
2021-06-27 17:11:08 +0530

    tha-10 updated

--- DAY-10/index.js
+++
@@ -1,8 +0,0 @@
-const gridContainer = document.querySelector('.game-container')
-for (let i = 1; i <= 12; i++) {
-    const divbox = document.createElement('div')
-    divbox.classList.add('box')
-    divbox.id = `${i}`
-    gridContainer.appendChild(divbox)
-}
-console.log('ndkjhn')
---
+++ DAY-10/logic.js
@@ -0,0 +1,106 @@
+let wordsarray = [
+    'red',
+    'orange',
+    'black',
+    'blue',
+    'green',
+    'purple',
+    'pink',
+    'brown',
+    'grey',
+]
+wordsarray = [...wordsarray, ...wordsarray]
+
+// DOM manupulation
+for (let i = 1; i <= 18; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    let cardinner = document.createElement('div')
+    cardinner.classList.add('card-inner')
+    let cardfront = document.createElement('div')
+    cardfront.classList.add('card-front')
+    let cardback = document.createElement('div')
+    let random = Math.floor(Math.random() * (18 - i))
+    cardback.innerHTML = '<h2>' + wordsarray[random] + '</h2>'
+    cardback.classList.add('card-back')
+    wordsarray.splice(random, 1)
+    cardinner.appendChild(cardfront)
+    cardinner.appendChild(cardback)
+    card.appendChild(cardinner)
+    document.querySelector('.game-container').appendChild(card)
+}
+// Utility functions
+const changeboard = (target, querytarget) => {
+    document.querySelector(querytarget).innerHTML = target
+}
+
+function flipcard(democard) {
+    democard.firstChild.classList.add('flipped')
+}
+
+function Unflipcard(democard) {
+    democard.firstChild.classList.remove('flipped')
+}
+
+function removecard(democard) {
+    democard.style.visibility = 'hidden'
+}
+
+function matchcards(firstcard, secondcard, score) {
+    let ismatch =
+        firstcard.firstChild.lastChild.innerHTML ===
+        secondcard.firstChild.lastChild.innerHTML
+    if (ismatch) {
+        score += 10
+        removecard(firstcard)
+        removecard(secondcard)
+    } else {
+        Unflipcard(firstcard)
+        Unflipcard(secondcard)
+    }
+    return score
+}
+
+function gamepopup(movescount, scorecount) {
+    if (movescount == 0) {
+        alert('gameover')
+    }
+    if (score === 90) {
+        alert('You Won')
+    }
+}
+
+// Game Logic
+// utility variables
+let moves = 40
+let score = 0
+let gamelock = true
+let firstcardclicked
+let cards = document.getElementsByClassName('card')
+document.getElementById('myBtn').addEventListener('click', function(e) {
+    gamelock = false
+})
+for (let card of cards) {
+    card.addEventListener('click', (e) => {
+        if (gamelock) {
+            return
+        } else {
+            moves--
+            changeboard(moves, '.movesnumber')
+            if (!firstcardclicked) {
+                firstcardclicked = card
+                flipcard(card)
+            } else {
+                flipcard(card)
+                gamelock = true
+                setTimeout(() => {
+                    score = matchcards(firstcardclicked, card, score)
+                    changeboard(score, '.scores')
+                    gamepopup(moves, score)
+                    firstcardclicked = undefined
+                    gamelock = false
+                }, 500)
+            }
+        }
+    })
+}
