dccc0f728bfed7f5fbf0668cc621c695b5e62f82
iemprashant <prashantgupta9876@gmail.com>
2021-07-03 18:17:41 +0530

    added loader

--- DAY-13/index.js
+++ DAY-13/index.js
@@ -1,10 +1,10 @@
+const loader = document.querySelector('#loading')
+
 function generateRandomVariable(i, max) {
     return Math.floor(Math.random() * (max - i))
 }
 
 function generateCard(quote, author) {
-    console.log(author)
-    console.log(quote)
     let card = document.createElement('div')
     card.classList.add('card')
     let quotestext = document.createElement('h3')
@@ -18,7 +18,7 @@ function generateCard(quote, author) {
     document.querySelector('.card-container').appendChild(card)
 }
 
-function generatehtml(arr) {
+function generateCards(arr) {
     let quotesarray = [...arr]
     for (var i = 0; i < 6; i++) {
         let randomvar = generateRandomVariable(i, 19 - i)
@@ -26,6 +26,7 @@ function generatehtml(arr) {
             quotesarray[randomvar]['content'],
             quotesarray[randomvar]['author']
         )
+        quotesarray.splice(randomvar, 1)
     }
 }
 
@@ -33,9 +34,39 @@ function getData() {
     fetch('https://api.quotable.io/quotes?page=1')
         .then((response) => response.json())
         .then((data) => {
-            generatehtml(data.results)
+            hideLoading()
+            generateCards(data.results)
         })
         .catch((error) => console.log('ERROR OCCURED'))
 }
 
+function displayLoading() {
+    loader.classList.add('display')
+        // to stop loading after some time
+    setTimeout(() => {
+        loader.classList.remove('display')
+    }, 5000)
+}
+
+function hideLoading() {
+    loader.classList.remove('display')
+}
+
+function getQod() {
+    fetch('https://quotes.rest/qod')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.contents.quotes[0].quote)
+        })
+        .catch((error) => console.log('ERROR OCCURED'))
+}
+
+document.getElementById('qodbtn').addEventListener('click', (e) => {
+    getQod()
+})
+
+document.getElementById('closebtn').addEventListener('click', function(e) {
+    console.log('close')
+})
+displayLoading()
 getData()
